5db2b0696215fc068719cae0bb5c318c538e4c79
Jakab-Mate <mate.jakab@ecolres.hu>
2024-06-28 02:09:42 +0200

    Update docs

--- make.jl
+++ make.jl
@@ -1,26 +1,33 @@
-push!(LOAD_PATH, "../src/")
+# Add the current project to LOAD_PATH
+push!(LOAD_PATH, "@.")
+
+# Load the necessary packages
 using MiCroSim, Documenter
 
+# Set up the environment
 ENV["GKSwstype"] = "100"
 
+# Generate path and base URL
 generated_path = joinpath(@__DIR__, "src")
 base_url = "https://github.com/Jakab-Mate/MiCroSim.jl/blob/main/"
 isdir(generated_path) || mkdir(generated_path)
 
+# Construct the correct path to README.md
+readme_path = joinpath(@__DIR__, "README.md")
+
+# Generate the readme.md file
 open(joinpath(generated_path, "readme.md"), "w") do io
-    println(
-        io,
-        """
-        ```@meta
-        EditURL = "$(base_url)README.md"
-        ```
-        """,
-    )
-    for line in eachline(joinpath(dirname(@__DIR__), "README.md"))
+    println(io, """
+    ```@meta
+    EditURL = "$(base_url)README.md"
+    ```
+    """)
+    for line in eachline(readme_path)
         println(io, line)
     end
 end
 
+# Generate the documentation
 makedocs(
     format = Documenter.HTML(),
     authors = "Jakab Máté",
@@ -36,4 +43,5 @@ makedocs(
     ]
 )
 
+# Deploy the documentation
 deploydocs(repo = "github.com/Jakab-Mate/MiCroSim.jl", push_preview = true)
---
+++ src/index.md
@@ -0,0 +1,11 @@
+## First function
+
+```@docs
+create_metabolism
+```
+
+## Second function
+
+```@docs
+create_species_pool
+```
