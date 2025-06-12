48aeac502e821501a5def2fd1f627450c0e8fe31
Jakab-Mate <mate.jakab@ecolres.hu>
2025-06-12 12:32:18 +0200

    Moved docs source to main branch

---
+++ docs/make.jl
@@ -0,0 +1,32 @@
+push!(LOAD_PATH, "../src/")
+using Pkg
+Pkg.activate(@__DIR__)
+Pkg.instantiate()
+
+using Patches
+using Documenter
+
+ENV["GKSwstype"] = "100"
+
+makedocs(
+    format = Documenter.HTML(inventory_version = "0.1"),
+    authors = "Jakab Máté",
+    sitename = "patch_model",
+    pages = [
+        "Home" => "readme.md",
+        "Manual" => "index.md",
+        "Tutorials" => Any[
+            "Baseline settings" => "example1.md",
+            "Order of introductions" => "example2.md"
+        ]
+    ],
+    repo = "https://github.com/Jakab-Mate/patch_model",
+    remotes = Dict(pwd() => Documenter.Remotes.GitHub("Jakab-Mate", "patch_model"))
+)
+
+deploydocs(
+    repo = "github.com/Jakab-Mate/patch_model.git",
+    branch = "gh-pages",
+    devbranch = "main",
+    target = "build"
+)
---
+++ docs/src/example1.md
@@ -0,0 +1,5 @@
+### Run 10 repeats with the default parameter settings
+
+```julia
+repeat_params(10, "path/to/your/destination")
+```
